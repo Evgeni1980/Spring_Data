8eb774e9aa1a73e77d3ab77bc043ef86a0c6ab9f
Evgeni <Kremeni@gmail.com>
2022-09-05 15:40:51 +0300

    Добавлено дз 7

---
+++ HomeWork_7/src/main/java/ru/kremenia/HomeWork7Application.java
@@ -0,0 +1,11 @@
+package ru.kremenia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWork7Application {
+    public static void main(String[] args) {
+        SpringApplication.run(HomeWork7Application.class, args);
+    }
+}
---
+++ HomeWork_7/src/main/java/ru/kremenia/persist/UserRepository.java
@@ -0,0 +1,17 @@
+package ru.kremenia.persist;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>  {
+    @Query(value = """
+            select * from users u 
+            where (:usernameFilter is null or u.username like :usernameFilter )
+            and (:emailFilter is null or u.email like :emailFilter )
+            """, nativeQuery = true)
+    List<User> usersByFilter(String usernameFilter, String emailFilter);
+}
